b711717a3fab60ef56c56b7da180449f6f808605
Adit <aditshah@berkeley.edu>
2023-07-18 18:13:55 +0200

    Refactor

--- code/simple_mlp.py
+++ code/simple_mlp.py
@@ -8,39 +8,41 @@ import torch.nn as nn
 import torch.nn.functional as F
 from utils import *
 
+
 class simple_mlp(nn.Module):
 
-	def __init__(self, feature_dim, layer, hidden):
-		
-		super(simple_mlp, self).__init__()
+    def __init__(self, feature_dim, layer, hidden):
 
-		self.layer = layer
+        super(simple_mlp, self).__init__()
 
-		self.linear1 = nn.Linear(feature_dim, hidden)
-		#self.bn1= nn.BatchNorm1d( hidden )		
+        self.layer = layer
 
-		if layer == 2:
-			self.linear2 = nn.Linear(hidden, hidden)
-			#self.bn2 = nn.BatchNorm1d( hidden )
+        self.linear1 = nn.Linear(feature_dim, hidden)
+        # self.bn1= nn.BatchNorm1d( hidden )
 
-		self.linear3 = nn.Linear(hidden, 1)	
-		#self.bn3 = nn.BatchNorm1d( 1 )
+        if layer == 2:
+            self.linear2 = nn.Linear(hidden, hidden)
+        # self.bn2 = nn.BatchNorm1d( hidden )
 
-	def forward(self, x, weights = None):
+        self.linear3 = nn.Linear(hidden, 1)
 
-		#hidden = F.tanh( self.bn1( self.linear1(x) ) )
-		#hidden = self.bn1 ( F.tanh( self.linear1(x) ) )
-		#hidden = F.relu( self.bn1 ( self.linear1(x) ) )
-		hidden = F.relu( self.linear1(x) )
+    # self.bn3 = nn.BatchNorm1d( 1 )
 
-		if self.layer == 2:
-			#hidden = F.tanh( self.bn2( self.linear2(hidden) ) )
-			#hidden = self.bn2( F.tanh( self.linear2(hidden) ) )
-			#hidden = F.relu( self.bn2( self.linear2(hidden) ) )
-			hidden = F.relu( self.linear2(hidden) )
+    def forward(self, x, weights=None):
 
-		#out = F.tanh( self.linear3(hidden) )
-		out = self.linear3(hidden)
-		#out = self.bn3(out)
+        # hidden = F.tanh( self.bn1( self.linear1(x) ) )
+        # hidden = self.bn1 ( F.tanh( self.linear1(x) ) )
+        # hidden = F.relu( self.bn1 ( self.linear1(x) ) )
+        hidden = F.relu(self.linear1(x))
 
-		return out, hidden
+        if self.layer == 2:
+            # hidden = F.tanh( self.bn2( self.linear2(hidden) ) )
+            # hidden = self.bn2( F.tanh( self.linear2(hidden) ) )
+            # hidden = F.relu( self.bn2( self.linear2(hidden) ) )
+            hidden = F.relu(self.linear2(hidden))
+
+        # out = F.tanh( self.linear3(hidden) )
+        out = self.linear3(hidden)
+        # out = self.bn3(out)
+
+        return out, hidden
